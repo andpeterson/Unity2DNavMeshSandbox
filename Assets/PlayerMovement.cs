89165a61a3e752a72fd19006b889828aa4e21572
Normsome <norm.ackroyd@gmail.com>
2019-10-14 16:59:00 -0700

    First Commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Vector2 destination;
+
+    public float moveSpeed = 5f;
+    public Animator animator;
+    private NavMeshAgent agent;
+    public Vector2 desitination;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 desitination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            desitination.z = 0;
+            agent.SetDestination(desitination);
+        }
+    }
+}
